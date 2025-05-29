f098c1b70803952d00e8b1d5527664ee4585a22d
Alx20002209 <alexkosev7@gmail.com>
2025-05-29 18:39:09 +0300

    Functionality is done need unexpected error handling and maybe smarter sorting

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,4 +1,5 @@
 from flask import request,jsonify, Flask
+from flask_restful import Api, Resource, fields, marshal_with
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
@@ -7,18 +8,21 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+api = Api(app)
 CORS(app)
 
 
 app.config["JWT_SECRET_KEY"] = "uoghwiuehgqeijfpowjjpowejpotjewjti3"
 jwt = JWTManager(app)
 
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///peachy.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class Transaction(db.Model):
+class TransactionModel(db.Model):
+    
     id = db.Column(db.Integer, primary_key = True)
     to_account = db.Column(db.String, unique = False, nullable = False)
     from_account = db.Column(db.String, unique = False, nullable = False)
@@ -36,7 +40,22 @@ class Transaction(db.Model):
             "date" : self.date
         }
 
+resource_fields = {
+    "id": fields.Integer,
+    "to_account" : fields.String,
+    "from_account": fields.String,
+    "ammount": fields.Float,
+    "state": fields.String,
+    "date": fields.String
+}
 
+class Transaction(Resource):
+    @marshal_with(resource_fields)
+    #@jwt_required()
+    def get(self, transaction_id):
+        result = TransactionModel.query.filter_by(id = transaction_id).first()
+        return result
+    
 @app.route("/api/token", methods=["POST"])
 def create_token():
     username = request.json.get("username", None)
@@ -49,12 +68,15 @@ def create_token():
 @app.route("/api/transactions", methods = ["GET"])
 @jwt_required()
 def get_transactions():
-    transactions = Transaction.query.all()
+    transactions = TransactionModel.query.all()
     json_transactions = list(map(lambda x: x.to_json(), transactions))
     return jsonify({"transactions":json_transactions})
 
+api.add_resource(Transaction,"/api/transactions/<int:transaction_id>")
+
+
 @app.route("/api/create_transaction",methods =["POST"])
-@jwt_required()
+#@jwt_required()
 def create_transaction():
     to_account = request.json.get('toAccount')
     from_account = request.json.get('fromAccount')
@@ -66,7 +88,7 @@ def create_transaction():
         return (
             jsonify({"message":"Please fill all the fields"}), 400,
         )
-    new_transaction = Transaction(to_account = to_account, from_account = from_account, ammount = ammount, state = state, date = date)
+    new_transaction = TransactionModel(to_account = to_account, from_account = from_account, ammount = ammount, state = state, date = date)
     try:
         db.session.add(new_transaction)
         db.session.commit()
@@ -76,15 +98,16 @@ def create_transaction():
     return jsonify({"message":"Transaction added"}), 201
 
 @app.route("/api/update_state/<int:id>", methods = ["PATCH"])
-@jwt_required()
+#@jwt_required()
 def update_state(id):
-    transaction = Transaction.query.get(id)
+    transaction = TransactionModel.query.get(id)
     
     if not transaction:
         return jsonify({"message":"Transaction not found"}), 404
     
     data = request.json
     transaction.state = data.get('state', transaction.state)
+    db.session.add(transaction)
     db.session.commit()
     return jsonify({'message':'State updated'}), 200
     
